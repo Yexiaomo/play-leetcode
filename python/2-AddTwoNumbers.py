b6e9f50ea2214bf728dc273cd73e52cc884e1b84
yexiaomo <markdownpad@163.com>
2019-10-05 22:11:57 +0800

    完成题目:2-两数相加

--- python/2-AddTwoNumbers.py
+++ python/2-AddTwoNumbers.py
@@ -9,9 +9,9 @@ class Solution:
         # 进行输入值判断
         if(l1 == None or l2 == None):
             return None
-        sum = l1.val + l2.val
-        resultList = ListNode(sum%10)
-        p = resultList
+        sum = l1.val + l2.val #两个数相加的和
+        resultList = ListNode(sum%10) #初始化结果链表
+        p = resultList #中间指针
         while(l1.next != None and l2.next != None):
             l1 = l1.next
             l2 = l2.next
@@ -19,20 +19,21 @@ class Solution:
             temp = ListNode(sum%10)
             p.next = temp
             p = p.next
+        # 当l1不为空
         while(l1.next != None):
             l1 = l1.next
             sum = l1.val + sum//10
             temp = ListNode(sum%10)
             p.next = temp
             p = p.next
-
+		# 当l2不为空
         while(l2.next != None):
             l2 = l2.next
             sum = l2.val + sum//10
             temp = ListNode(sum%10)
             p.next = temp
             p = p.next
-
+		#还有进位时
         if(sum//10 == 1):
             temp = ListNode(1)
             p.next = temp
