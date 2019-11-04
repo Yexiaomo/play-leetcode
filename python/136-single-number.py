5d16b4b8cdbda810f7c9976a519f29790b0478a3
yexiaomo <markdownpad@163.com>
2019-11-04 19:22:32 +0800

    打卡第三十一天: 题目136.只出现一次的数字

--- python/136-single-number.py
+++ python/136-single-number.py
@@ -1,6 +1,10 @@
 class Solution:
+    #第一种 利用位运算,异或运算符
     def singleNumber(self, nums: List[int]) -> int:
         flag = 0
         for n in nums:
             flag ^= n
         return flag
+    #第二种 利用数学的思想: 2*(a+b)-(a+b+a) = b
+    def singleNumber(self, nums: List[int]) -> int:
+        return 2*(sum(set(nums))) - sum(nums)
