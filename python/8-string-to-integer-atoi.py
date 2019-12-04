db0360c5caf3cdb53f6a57696d6f59bb4ab5e7eb
Yexiaomo <Yexiaomo@users.noreply.github.com>
2019-12-04 13:38:06 +0800

    腾讯计划打卡第四天: 题目5. 最长回文子串

--- python/8-string-to-integer-atoi.py
+++ python/8-string-to-integer-atoi.py
@@ -28,3 +28,45 @@ class Solution:
         if(num < -2**31):
             return -2**31
         return num
+    
+''' 腾讯计划 2019/12/04
+    def myAtoi(self, str: str) -> int:
+        # 因为只需要判断字符串中第一个子串中的前整数部分
+        subStr = str.split()
+        if(subStr == []):
+            return 0
+        else:
+            subStr = subStr[0]
+        
+        #提取正负号
+        i = 0
+        if(subStr[0] == '-'):
+            i = 1
+            flag = -1
+        elif(subStr[0] == '+'):
+            i = 1
+            flag = +1
+        elif(subStr[0].isdigit()):
+            flag = +1
+        else:
+            return 0
+        
+        #获得数字子串
+        ans = ''
+        while(i<len(subStr)):
+            if(subStr[i].isdigit()):
+                ans += subStr[i]
+                i+=1
+            else:
+                break
+        if(ans == ''): return 0
+        
+        #对结果进行判别
+        result = int(ans) * flag
+        if(result >= 2**31):
+            return (2**31-1)
+        if(result < -2**31):
+            return -2**31
+        
+        return result
+'''
