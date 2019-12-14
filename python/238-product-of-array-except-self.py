4bd98676bbe796aae26d499b25a196b6ba148ab3
Yexiaomo <Yexiaomo@users.noreply.github.com>
2019-12-14 22:17:01 +0800

    腾讯计划-打卡第十四天- 238. 除自身以外数组的乘积

--- python/238-product-of-array-except-self.py
+++ python/238-product-of-array-except-self.py
@@ -1,4 +1,19 @@
 class Solution:
+    # 改良版: 最后的输出数组使用map函数
+    '''思路: 定义两个数组
+        leftNumsProduct保存当前位置元素左边的乘积
+        rightNumsProduct保存当前位置元素右边的乘积
+    '''
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        len_nums = len(nums)
+        leftNumsProduct = [1]*len_nums
+        for i in range(1, len_nums):
+            leftNumsProduct[i] = leftNumsProduct[i-1]* nums[i-1]
+        rightNumsProduct = [1]*len_nums
+        for i in range(len_nums-2, -1, -1):
+            rightNumsProduct[i] = rightNumsProduct[i+1]* nums[i+1]
+        return map(lambda a,b: a*b, leftNumsProduct, rightNumsProduct)
+
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         #定义两个数组, 分别保存当前位置元素的左边乘积,和右边乘积
         #后面两个for循环可以写成一个
