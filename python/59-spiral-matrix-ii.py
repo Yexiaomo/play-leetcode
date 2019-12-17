ce6b7a7d10641b231cfbd4b840ff99f8c89577f2
Yexiaomo <Yexiaomo@users.noreply.github.com>
2019-12-17 13:22:57 +0800

    腾讯计划-打卡第十七天-59. 螺旋矩阵 II

--- python/59-spiral-matrix-ii.py
+++ python/59-spiral-matrix-ii.py
@@ -26,3 +26,41 @@ class Solution:
                 num+=1
             l+=1
         return rst
+
+    # 腾讯计划, 2019/12/17, 方法不是很好
+    def generateMatrix(self, n: int):
+        if(n<=0): return []
+        if(n==1): return [[1]]
+        circls = (n-1)//2 + 1
+        nums = 1
+        output = [[0 for i in range(n)] for i in range(n)]
+        ml = nl = 0
+        mr = nr = n-1
+        for i in range(circls): 
+            mi = ni = i
+            # 顶部, 从左到右
+            while(ni < nr):
+                output[mi][ni] = nums
+                nums+=1
+                ni+=1
+            # 右侧, 从上到下到右
+            while(mi < mr):
+                output[mi][ni] = nums
+                nums+=1
+                mi+=1
+            # 底部.从右到左
+            while(ni > nl):
+                output[mi][ni] = nums
+                nums+=1
+                ni-=1
+            # 左侧,从下到上
+            while(mi > ml):
+                output[mi][ni] = nums
+                nums+=1
+                mi-=1
+            # 更新边界值
+            ml = nl = ml+1
+            mr = nr = mr-1
+        # 处理特殊情况
+        if(n %2 != 0): output[mi][ni] = nums
+        return output
