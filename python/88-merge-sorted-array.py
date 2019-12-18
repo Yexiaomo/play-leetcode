5b95f42d20f1a6e556f05737b875885f6de27ae5
Yexiaomo <Yexiaomo@users.noreply.github.com>
2019-12-18 12:09:34 +0800

    腾讯计划-打卡第十八天-88. 合并两个有序数组

--- python/88-merge-sorted-array.py
+++ python/88-merge-sorted-array.py
@@ -1,4 +1,33 @@
 class Solution:
+    # 腾讯计划,改良版
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        index = m+n-1
+        m-=1
+        n-=1
+        while(m >= 0 and n >= 0):
+            if(nums1[m] >= nums2[n]):
+                nums1[index] = nums1[m]
+                m-=1
+            else:
+                nums1[index] = nums2[n]
+                n-=1
+            index-=1
+        # while(n>=0):
+        #     nums1[index] = nums2[n]
+        #     n-=1
+        #     index-=1
+        # while(m>=0):
+        #     nums1[index] = nums1[m]
+        #     m-=1
+        #     index-=1
+        # 上面的代码没有考虑到,如果nums1中元素还没有复制到目标数组,但是目标数组就是nums1就不用复制了
+        # 所以只需要考虑nums2数组是不是已经拷贝完就可以了
+        nums1[:n+1] = nums2[:n+1]
+
+
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
