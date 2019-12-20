083ba2de7415fe89a9f53f34dc2b83429225648e
Yexiaomo <Yexiaomo@users.noreply.github.com>
2019-12-20 13:42:26 +0800

    腾讯计划-打卡第二十天-2.两数相加

--- python/2-AddTwoNumbers.py
+++ python/2-AddTwoNumbers.py
@@ -5,44 +5,31 @@
 #         self.next = None
 
 class Solution:
+    # 因为是非负整数，不用考虑正负号
+    # 每次注意加上进位就ok了
+    # 注意判断是不是已经到头了
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        # 进行输入值判断
-        if(l1 == None or l2 == None):
-            return None
-        sum = l1.val + l2.val #两个数相加的和
-        resultList = ListNode(sum%10) #初始化结果链表
-        p = resultList #中间指针
-        while(l1.next != None and l2.next != None):
+        flag = 0#代表进位
+        l3 = ListNode(0)
+        p = l3
+        while(l1 and l2):
+            tmp = ListNode(l1.val+l2.val+ flag)
+            tmp.val, flag = tmp.val % 10, tmp.val // 10
+            p.next = tmp
+            p = p.next
             l1 = l1.next
             l2 = l2.next
-            sum = l1.val + l2.val + sum//10
-            temp = ListNode(sum%10)
-            p.next = temp
+        if(l1): p.next = l1
+        if(l2): p.next = l2
+        while(p.next):
+            p.next.val += flag
+            p.next.val, flag = p.next.val % 10, p.next.val // 10
             p = p.next
-        # 当l1不为空
-        while(l1.next != None):
-            l1 = l1.next
-            sum = l1.val + sum//10
-            temp = ListNode(sum%10)
-            p.next = temp
-            p = p.next
-		# 当l2不为空
-        while(l2.next != None):
-            l2 = l2.next
-            sum = l2.val + sum//10
-            temp = ListNode(sum%10)
-            p.next = temp
-            p = p.next
-		#还有进位时
-        if(sum//10 == 1):
-            temp = ListNode(1)
-            p.next = temp
-            p = p.next
-        return resultList
+        if(flag == 1):
+            p.next = ListNode(1)
+        return l3.next
 
-
-# 改进后的代码
-
+# 改进代码
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         # 进行输入值判断
@@ -60,9 +47,7 @@ class Solution:
             temp = ListNode(sum%10)
             p.next = temp
             p = p.next
-            
         #还有进位时
         if(sum//10 == 1):
             p.next = ListNode(1)
-
         return resultList
