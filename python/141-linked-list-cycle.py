758c583adfbb52d12b3489696795f69524d3f8fe
Yexiaomo <Yexiaomo@users.noreply.github.com>
2019-12-24 15:50:41 +0800

    腾讯计划-打卡第二十三天-141. 环形链表

--- python/141-linked-list-cycle.py
+++ python/141-linked-list-cycle.py
@@ -26,4 +26,20 @@ class Solution:
                 ans.add(head)
                 head = head.next
         return False
-            
+    
+    # 看了知识点，标签是双指针，但这又是链表，所以这个双指针就应该是快慢指针
+    # 又因为判断是否有环，所以如果存在环，那么两个指针一定会遇见
+    # 但是，如果没有环，怎么判断呢。。。显然，快指针指向的元素不能为空
+    # OK，主要就是双指针移动
+    def hasCycle(self, head: ListNode) -> bool:
+        slow = fast = head
+        flag = 0
+        while(fast):
+            fast = fast.next
+            if(fast == slow):
+                return True
+            flag += 1
+            if(flag == 2):
+                flag = 0
+                slow = slow.next
+        return False
