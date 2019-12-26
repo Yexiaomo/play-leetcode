43a3c15658a92be7bf1b8b919374d481fc8ea2f4
Yexiaomo <Yexiaomo@users.noreply.github.com>
2019-12-26 15:15:34 +0800

    腾讯计划-打卡第二十六天-160. 相交链表

--- python/160-intersection-of-two-linked-lists.py
+++ python/160-intersection-of-two-linked-lists.py
@@ -4,6 +4,7 @@
 #         self.val = x
 #         self.next = None
 class Solution(object):
+    #很巧妙,推导一下便可知
     def getIntersectionNode(self, headA, headB):
         """
         :type head1, head1: ListNode
@@ -16,3 +17,20 @@ class Solution(object):
             pA = pA.next if(pA) else headB
             pB = pB.next if(pB) else headA
         return pA
+
+    #集合
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        # 因为,是判断两个链表是否相交
+        # 所以,用先遍历一遍一个链表,存入集合中
+        # 遍历第二个链表,判断是否在集合中
+        # 如果
+        ans = set()
+        while(headA):
+            ans.add(headA)
+            headA = headA.next
+        if(ans == None): return None
+        while(headB):
+            if(headB in ans):
+                return headB
+            headB = headB.next
+        return None
