55371b5ccbdc78c6d65fa9233b9b59873051784b
Yexiaomo <Yexiaomo@users.noreply.github.com>
2019-12-29 09:19:25 +0800

    腾讯计划-打卡第二十九天-9.回文数

--- python/9-palindrome-number.py
+++ python/9-palindrome-number.py
@@ -1,15 +1,16 @@
 class Solution:
+    #反转整数后面一半就够了
+    #负数,被十整除的数属于特殊情况
     def isPalindrome(self, x: int) -> bool:
-        if(x == None or x<0): return False
-        x = str(x)
-        xLen = len(x)
-        for i in range(xLen//2):
-            if(x[i] != x[xLen-i-1]):
-                return False
-        return True
-
-# 改进后的代码, 人生苦短,我用Python
-class Solution:
+        if(x < 0 or (x % 10 == 0 and x != 0)): return False
+        xRight = 0
+        while(xRight < x):
+            xRight = x%10 + xRight*10
+            x = x//10
+        return xRight == x or xRight//10 == x
+    
+    #下面属于违规操作,使用了字符串
+    # 改进后的代码, 人生苦短,我用Python
     def isPalindrome(self, x: int) -> bool:
         x = str(x)
         return True if(x == x[::-1]) else False
