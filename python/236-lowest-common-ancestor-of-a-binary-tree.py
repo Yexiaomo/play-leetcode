6344ee1282196ec8a23352dacdfdbb99c5724e10
yexiaomo <markdownpad@163.com>
2020-01-09 21:25:06 +0800

    腾讯计划-打卡第四十天-236. 二叉树的最近公共祖先

--- python/236-lowest-common-ancestor-of-a-binary-tree.py
+++ python/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -13,3 +13,9 @@ class Solution:
         left=self.lowestCommonAncestor(root.left,p,q) #去左子树看是否可以访问到p或q,并把结果返回给left
         right=self.lowestCommonAncestor(root.right,p,q) #去右子树看是否可以访问到p或q,并把结果返回给right
         return root if left and right else left or right #如果left,right都不为空,则说明在左子树和右子树都分别访问到了p或q,则此时root是最近公共祖先,否则,哪个不空则返回哪个
+   
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if(root in [None, p, q]): return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        return right if(left == None) else left if(right == None) else root
