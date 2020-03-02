21d337f0cca15228d0d93b29955085731924d28c
Yexiaomo <markdownpad@163.com>
2020-03-02 12:03:36 +0800

    剑指offer打卡第二天

--- JianZhi-offer/04-er-wei-shu-zu-zhong-de-cha-zhao-lcof.java
+++ JianZhi-offer/04-er-wei-shu-zu-zhong-de-cha-zhao-lcof.java
@@ -1,33 +1,17 @@
 class Solution {
-    //方法①: 使用集合法, 空间复杂度高
-    // public int findRepeatNumber(int[] nums) {
-    //     Set ansSet = new HashSet();
-    //     for(int n : nums){
-    //         if( ansSet.contains(n) ){
-    //             return n;
-    //         }else{
-    //             ansSet.add(n);
-    //         }
-    //     }
-    //     return -1;
-    // }
-    //方法②: 使用原地排序法
-    public int findRepeatNumber(int[] nums) {
-        int tmp;
-        for(int i = 0; i < nums.length; i++){
-            while(nums[i] != i){
-                if(nums[i] == nums[nums[i]]){
-                    return nums[i];
-                }
-                // tmp = nums[i];
-                // nums[i] = nums[tmp];
-                // nums[tmp] = tmp;
-                //利用位运算进行交换
-                nums[i] = nums[i] ^ nums[nums[i]];
-                nums[nums[i]] = nums[i] ^ nums[nums[i]];
-                nums[i] = nums[i] ^ nums[nums[i]];
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if(matrix==null || matrix.length==0)
+            return false;
+        int i = matrix.length - 1, j = 0;
+        while(i > -1 && j < matrix[0].length){
+            if(matrix[i][j] > target){
+                i-=1;
+            }else if(matrix[i][j] < target){
+                j+=1;
+            }else{
+                return true;
             }
         }
-        return -1;
+        return false;
     }
 }
