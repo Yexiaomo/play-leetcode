124f4daf8594948e17a05fdf7c781cbaf2569da5
Yexiaomo <markdownpad@163.com>
2020-05-10 13:10:33 +0800

    66. 构建乘积数组

--- JianZhi-offer/66-gou-jian-cheng-ji-shu-zu-lcof.java
+++ JianZhi-offer/66-gou-jian-cheng-ji-shu-zu-lcof.java
@@ -5,12 +5,10 @@ class Solution {
 
         int len = a.length;
         int[] b = new int[len];
-
         //统计当前下标左边的乘积
         b[0] = 1;
         for(int i = 1; i < len; i++)
             b[i] = b[i-1] * a[i-1];
-
         //统计当前下标右边的乘积
         //b[i] = 当前下标左边乘积 * 当前下标右边乘积;
         for(int i = len-2, tmp = 1; i >= 0; i--){
